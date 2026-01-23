1fdf6df4606025a361f0f6b7609a740ed89f24ba
Lithira-Jayanaka <kariyawasamlithira2@gmail.com>
2026-01-23 22:43:11 +0530

    initialize NativeWind for styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import "../global.css"
 
 const Index = () => {
   return (
-    <View>
-      <Text>Index</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to HabitFlux !
+      </Text>
     </View>
   )
 }
