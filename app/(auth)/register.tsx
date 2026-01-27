c74b084eca4c692137bd2e90e485f912bf256edf
Lithira-Jayanaka <kariyawasamlithira2@gmail.com>
2026-01-27 15:33:50 +0530

    enhance registration screen with input fields and navigation to login

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,12 +1,85 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  Keyboard,
+  TouchableWithoutFeedback,
+  TextInput,
+  TouchableOpacity,
+  Pressable,
+} from "react-native";
+import React, { useState } from "react";
+import { useRouter } from "expo-router";
+import { useLoader } from "@/hooks/useLoader";
 
 const Register = () => {
-  return (
-    <View className="flex-1 justify-center items-center bg-gray-50 p-6">
-      <Text>Register</Text>
-    </View>
-  )
-}
+  const router = useRouter();
 
-export default Register
+  const { showLoader, hideLoader, isLoading } = useLoader();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleRegister = async () => {
+    // Registration logic will go here
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View className="flex-1 justify-center items-center bg-gray-50 p-6">
+        <View className="w-full bg-white/50 backdrop-blur-md rounded-2xl shadow-lg p-8">
+          <Text className="text-3xl font-bold mb-6 text-center text-gray-900">
+            Register
+          </Text>
+          <TextInput
+            placeholder="name"
+            placeholderTextColor="#6B7280"
+            className="border bg-gray-300 p-3 mb-4 rounded-xl"
+            value={name}
+            onChangeText={setName}
+          />
+          <TextInput
+            placeholder="email"
+            placeholderTextColor="#6B7280"
+            className="border bg-gray-300 p-3 mb-4 rounded-xl"
+            value={email}
+            onChangeText={setEmail}
+          />
+          <TextInput
+            placeholder="password"
+            placeholderTextColor="#6B7280"
+            className="border bg-gray-300 p-3 mb-4 rounded-xl"
+            value={password}
+            onChangeText={setPassword}
+          />
+          <TextInput
+            placeholder="confirm password"
+            placeholderTextColor="#6B7280"
+            className="border bg-gray-300 p-3 mb-4 rounded-xl"
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+          />
+          <Pressable
+            className="bg-blue-600/80 px-6 py-3 rounded-2xl"
+            onPress={handleRegister}
+          >
+            <Text className="text-white text-lg text-center">Register</Text>
+          </Pressable>
+          <View className="flex-row justify-center mt-2">
+            <Text className="text-gray-700">Alrady have an account? </Text>
+            <TouchableOpacity
+              onPress={() => {
+                router.back()
+              }}
+            >
+              <Text className="text-blue-600 font-semibold">Login</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+export default Register;
