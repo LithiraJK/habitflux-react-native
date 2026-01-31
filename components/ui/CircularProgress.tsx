bbe180addf8d02f3951d53de7e836d8187a0df6c
Lithira-Jayanaka <kariyawasamlithira2@gmail.com>
2026-01-31 23:57:05 +0530

    add CircularProgress component for displaying progress in a circular format

---
+++ components/ui/CircularProgress.tsx
@@ -0,0 +1,37 @@
+
+import { View } from 'react-native';
+import Svg, { Circle } from 'react-native-svg';
+
+const CircularProgress = ({ progress, size = 280, strokeWidth = 15, children }: any) => {
+    const radius = (size - strokeWidth) / 2;
+    const circumference = radius * 2 * Math.PI;
+    const strokeDashoffset = circumference - (progress * circumference);
+
+    return (
+        <View className="justify-center items-center">
+            <Svg width={size} height={size}>
+                
+                <Circle
+                    cx={size / 2} cy={size / 2} r={radius}
+                    stroke="#333" strokeWidth={strokeWidth} fill="transparent"
+                />
+                
+                <Circle
+                    cx={size / 2} cy={size / 2} r={radius}
+                    stroke="#8B5CF6" strokeWidth={strokeWidth}
+                    strokeDasharray={circumference}
+                    strokeDashoffset={strokeDashoffset}
+                    strokeLinecap="round"
+                    fill="transparent"
+                    rotation="-90"
+                    origin={`${size / 2}, ${size / 2}`}
+                />
+            </Svg>
+            <View className="absolute justify-center items-center">
+                {children}
+            </View>
+        </View>
+    );
+};
+
+export default CircularProgress;
